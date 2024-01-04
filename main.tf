a5ec1bc4c3d5af08284ef9017a01530e38ce0a3e
CodingManoj <CodingManoj@gmail.com>
2024-01-04 07:41:36 +0530

    TF VPC Auto Push

---
+++ main.tf
@@ -0,0 +1,10 @@
+module "docdb" {
+  source              = "./vendor/modules/docdb"
+
+  ENV                 = var.ENV
+}
+
+
+
+# We cannot parametrize anything that's added n the source.
+# To limit that, we can use a tool called as Terrafile. All it does is clonnes the remote code from the specified branch and make it locally available.
