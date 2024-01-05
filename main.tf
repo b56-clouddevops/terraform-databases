5c9e25e033022597580ecc1faaea2529dd5c0916
CodingManoj <CodingManoj@gmail.com>
2024-01-05 06:50:11 +0530

    TF VPC Auto Push

--- main.tf
+++ main.tf
@@ -5,7 +5,12 @@ module "docdb" {
   DOCDB_PORT          = var.DOCDB_PORT
 }
 
+module "mysql" {
+  source              = "./vendor/modules/mysql"
 
+  ENV                 = var.ENV
+  DOCDB_PORT          = var.MYSQL_PORT
+}
 
 # We cannot parametrize anything that's added n the source.
 # To limit that, we can use a tool called as Terrafile. All it does is clonnes the remote code from the specified branch and make it locally available.
