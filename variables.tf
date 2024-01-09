5e4f436eae8f6f31afbcf3b030a07a362e7c0e36
CodingManoj <CodingManoj@gmail.com>
2024-01-09 06:43:53 +0530

    TF DB Auto Push

--- variables.tf
+++ variables.tf
@@ -13,6 +13,5 @@ variable "MYSQL_PORT" {}
 variable "REDIS_PORT" {}
 variable "REDIS_ENGINE" {}
 variable "REDIS_ENGINE_FAMILY" {}
-variable "REDIS_PORT" {}
 variable "REDIS_ENGINE_VERSION" {}
 variable "REDIS_INSTANCE_COUNT" {}
